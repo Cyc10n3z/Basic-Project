28f6f98d4fc3e05a8c53afcf8f4342ce033de15f
John Lorette III <JLo42@live.missouristate.edu>
2022-02-22 19:17:38 -0600

    Part 1: Warm-Up
    Added the quintessential "Hello World".

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,6 +1,7 @@
 package;
 
 import flixel.FlxState;
+import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
@@ -8,6 +9,8 @@ class PlayState extends FlxState
 	{
 		super.create();
 		add(new Hero(300, 300));
+		// Added a new FlxText object and modified it's default appearance
+		var text = add(new FlxText(300, 200, "Hello World!").setFormat("arial", 14, 0xFFFF0000, "center", OUTLINE, 0xFF00FFFF));
 	}
 
 	override public function update(elapsed:Float)
